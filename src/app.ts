83bda9212bba1450fd72741051e76692722d84dd
Jared Hall <hall.jared.c@gmail.com>
2023-02-01 08:05:20 -0600

    A beginning to routing

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,10 @@ import './style/milligram.sass'
 import './style/app.sass'
 import '@riotjs/hot-reload'
 import App from './components/app'
-import { component } from 'riot'
+import { Route, Router, URL } from '@riotjs/route/route.js'
+import { component, register } from 'riot'
+
+register('router', Router)
+register('route', Route)
 
 component(App)(document.body, {})
